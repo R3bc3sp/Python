f9c852cf6274862f88a3438a897750b46c91a7a1
Ivan Zhen <ivangpajon@hotmail.com>
2018-01-15 18:34:42 +0100

    Finished Actividad 3.5

--- primerProyectoPython/Actividad35/Ejercicio2.py
+++ primerProyectoPython/Actividad35/Ejercicio2.py
@@ -1,15 +1,31 @@
 # ‐*‐ coding: utf‐8 ‐*‐
 
-import datetime as dt
+import datetime as dt, time
 
 def abrir_log(nombre):
     archivo = open(nombre, "a")
     fecha = str(dt.datetime.now())
-    mensaje = fecha + ": Iniciando registro de errores"
+    mensaje = fecha + ": Iniciando registro de errores\n"
     archivo.write(mensaje)
     return archivo
 
 def guardar_log(archivo, mensaje):
-    
+    fecha = str(dt.datetime.now())
+    mensaje = fecha + ": " + mensaje + "\n"
+    archivo.write(mensaje)
 
-abrir_log("log.txt")
+def cerrar_log(archivo):
+    fecha = str(dt.datetime.now())
+    mensaje = fecha + ": Finalizando registro de errores\n"
+    archivo.write(mensaje)
+    archivo.close()
+
+archivo_log = abrir_log("log.txt")
+time.sleep(5)
+guardar_log(archivo_log, "Primer mensaje")
+time.sleep(1)
+guardar_log(archivo_log, "Segundo mensaje mensaje")
+time.sleep(1)
+guardar_log(archivo_log, "Ultimo mensaje")
+time.sleep(5)
+cerrar_log(archivo_log)
