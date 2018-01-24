62f790c3a06400a49e7390f9c0ceb319ef12e9f6
21609121 <21609121@N209-10.alumnos.uem.es>
2018-01-24 08:46:57 +0100

    Finished Actividad 3.6

--- primerProyectoPython/Actividad36/Ejercicio1.py
+++ primerProyectoPython/Actividad36/Ejercicio1.py
@@ -1,23 +1,23 @@
 # ‐*‐ coding: utf‐8 ‐*‐
 
-def calcularFibonacciSinExcepciones(n):
-    if n >= 20 or n <= 0:
+def calcularFibonacciConExcepciones(n):
+    try:
+        assert not(n <= 0 or n >= 20)
+        salida=[]
+        a,b = 0,1
+        for x in range(n):
+            salida.append(b)
+            a, b = b, a+b
+    except AssertionError:
         print "Ha ingresado un valor incorrecto. El valor debe ser un número entero mayor que 0 y menor que 20"
-        return
-    
-    salida=[]
-    a,b = 0,1
-    for x in range(n):
-        salida.append(b)
-        a, b = b, a+b
-    
+
     return salida
 
-def mainSinExcepciones():
+def mainConExcepciones():
     try:
         n = input("Ingrese n para calcular Fibonacci: ")
-        print calcularFibonacciSinExcepciones(n)
-    except NameError as e:
+        print calcularFibonacciConExcepciones(n)
+    except NameError:
         print "Introduce un digito"
 
-mainSinExcepciones()
+mainConExcepciones()
